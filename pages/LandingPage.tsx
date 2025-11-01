b8979b5d375d7964deb7ca2882350a48d0ae4fc9
Hassan010103 <shamsihassan002@gmail.com>
2025-11-01 22:14:36 +0530

    feat: Use placeholder images for landing page slideshow
    Switches the landing page slideshow to use placeholder images. This change ensures the application can be deployed and tested while local images are still being prepared.

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -70,7 +70,7 @@ const localImages = [
 ];
 
 // Switch this line to use your local images when they are ready.
-const slideshowImages = localImages;
+const slideshowImages = placeholderImages;
 
 
 const LandingPage: React.FC = () => {
